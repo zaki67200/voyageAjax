e48f1489aea075c7f722bb5ef65f327f83e80009
zaki67200 <zakaria.arahouani@gmail.com>
2020-03-30 13:23:47 +0200

    exercice voyage avec CSS

--- voyage.js
+++ voyage.js
@@ -16,51 +16,56 @@ window.addEventListener("DOMContentLoaded", function(event) {
 
                     {
                         apiList1.innerHTML +=
-                                              '<h1>' + json[0].titre +'</h1>'+
-                                              '<h2>' + json[0].depart +'</h2>'+
-                                              '<h2>' + json[0].destination +'</h2>'+
-                                              '<h2>' + json[0].nombre_jours +'</h2>'+
-                                              '<h2>' + json[0].prix +'</h2>'+
-                                              '<img src="newyork.jpg" alt="NewYork" >'+
-                                              '<p>' + json[0].description +'</p>';
+                                              '<div class ="titre"><h1>' + json[0].titre +'</h1>'+
+                                              '<h2>' +'Départ de'+' '+ json[0].depart +'</h2>'+
+                                              '<h2>' +'Destination'+' '+ json[0].destination +'</h2>'+
+                                              '<h3>' + json[0].nombre_jours +' '+'jours'+'</h3>'+
+                                              '<h3>' + json[0].prix +' '+'€'+'</h3></div>'+
+                                              '<div class ="articleBDI1">'+'</div>'+
+                                              '<p>' + json[0].description +'</p>'+
+                                               '<button class="favorite" type="button">'+'Lire la suite'+'</button>';
 
 
 
                                               apiList2.innerHTML +=
-                                                                    '<h1>' + json[1].titre +'</h1>'+
-                                                                    '<h2>' + json[1].depart +'</h2>'+
-                                                                    '<h2>' + json[1].destination +'</h2>'+
-                                                                    '<h2>' + json[1].nombre_jours +'</h2>'+
-                                                                    '<h2>' + json[1].prix +'</h2>'+
-                                                                    '<img src="marakech.jpg" alt="Marakech" >'+
-                                                                    '<p>' + json[1].description +'</p>';
+                                                                    '<div class ="titre"><h1>' + json[1].titre +'</h1>'+
+                                                                    '<h2>' +'Départ de'+' '+ json[1].depart +'</h2>'+
+                                                                    '<h2>' +'Destination'+' '+ json[1].destination +'</h2>'+
+                                                                    '<h3>' + json[1].nombre_jours +' '+'jours'+'</h3>'+
+                                                                    '<h3>' + json[1].prix +' '+'€'+'</h3></div>'+
+                                                                    '<div class ="articleBDI2">'+'</div>'+
+                                                                    '<p>' + json[1].description +'</p>'+
+                                                                    '<button class="favorite" type="button">'+'Lire la suite'+'</button>';
                                             apiList3.innerHTML +=
-                                                                    '<h1>' + json[2].titre +'</h1>'+
-                                                                    '<h2>' + json[2].depart +'</h2>'+
-                                                                    '<h2>' + json[2].destination +'</h2>'+
-                                                                    '<h2>' + json[2].nombre_jours +'</h2>'+
-                                                                    '<h2>' + json[2].prix +'</h2>'+
-                                                                    '<img src="reunion.jpg" alt="Reunion" >'+
-                                                                    '<p>' + json[2].description +'</p>';
+                                                                    '<div class ="titre"><h1>' + json[2].titre +'</h1>'+
+                                                                    '<h2>' +'Départ de'+' '+ json[2].depart +'</h2>'+
+                                                                    '<h2>' +'Destination'+' '+ json[2].destination +'</h2>'+
+                                                                    '<h3>' + json[2].nombre_jours +' '+'jours'+'</h3>'+
+                                                                    '<h3>' + json[2].prix +' '+'€'+'</h3></div>'+
+                                                                    '<div class ="articleBDI3">'+'</div>'+
+                                                                    '<p>' + json[2].description +'</p>'+
+                                                                    '<button class="favorite" type="button">'+'Lire la suite'+'</button>';
 
                                             apiList4.innerHTML +=
-                                                                    '<h1>' + json[3].titre +'</h1>'+
-                                                                    '<h2>' + json[3].depart +'</h2>'+
-                                                                    '<h2>' + json[3].destination +'</h2>'+
-                                                                    '<h2>' + json[3].nombre_jours +'</h2>'+
-                                                                    '<h2>' + json[3].prix +'</h2>'+
-                                                                    '<img src="tchernobly.jpg" alt="Tchernobly" >'+
-                                                                    '<p>' + json[3].description +'</p>';
+                                                                    '<div class ="titre"><h1>' + json[3].titre +'</h1>'+
+                                                                    '<h2>'+'Départ de'+' ' + json[3].depart +'</h2>'+
+                                                                    '<h2>' +'Destination'+' '+ json[3].destination +'</h2>'+
+                                                                    '<h3>' + json[3].nombre_jours +' '+'jours'+'</h3>'+
+                                                                    '<h3>' + json[3].prix +' '+'€'+'</h3></div>'+
+                                                                    '<div class ="articleBDI4">'+'</div>'+
+                                                                    '<p>' + json[3].description +'</p>'+
+                                                                    '<button class="favorite" type="button">'+'Lire la suite'+'</button>';
 
 
                                             apiList5.innerHTML +=
-                                                                    '<h1>' + json[4].titre +'</h1>'+
-                                                                    '<h2>' + json[4].depart +'</h2>'+
-                                                                    '<h2>' + json[4].destination +'</h2>'+
-                                                                    '<h2>' + json[4].nombre_jours +'</h2>'+
-                                                                    '<h2>' + json[4].prix +'</h2>'+
-                                                                   '<img src="athenes.jpg" alt="Athenes" >'+
-                                                                  '<p>' + json[4].description +'</p>';
+                                                                    '<div class ="titre"><h1>' + json[4].titre +'</h1>'+
+                                                                    '<h2>'+'Départ de'+' '+ json[4].depart +'</h2>'+
+                                                                    '<h2>'+'Destination'+' '+json[4].destination +'</h2>'+
+                                                                    '<h3>' + json[4].nombre_jours +' '+'jours'+'</h3>'+
+                                                                    '<h3>' + json[4].prix +' '+'€'+'</h3></div>'+
+                                                                    '<div class ="articleBDI5">'+'</div>'+
+                                                                    '<p>' + json[1].description +'</p>'+
+                                                                    '<button class="favorite" type="button">'+'Lire la suite'+'</button>';
 
 
 
